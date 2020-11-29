20d7ff5f529da21b295834f24d6ca868d619ddc4
arnog <arno@arno.org>
2020-11-29 09:20:13 +0100

    chore: build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,13 @@ set -o pipefail  # don't hide errors within pipes
 
 cd "$(dirname "$0")/.."
 
+# If no "node_modules" directory, do an install first
+if [ ! -d "./node_modules" ]; then
+    printf "\033[32m\033[1K ● \033[0Installing dependencies"
+    npm install
+    echo -e "\033[32m\033[1K ✔ \033[0Dependencies installed"
+fi
+
 # Read the first argument, set it to "dev" if not set
 export BUILD="${1-dev}"
 
